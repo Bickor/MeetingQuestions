7525386a87f72ac2e5d6015f98fa2e7dd7dbc28b
Martin Heberling <martin.heberling.dalies@gmail.com>
2021-10-21 11:44:29 -0400

    Added textbox for questions and GTSHPE logo.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
+import logo from './shpe_logo.jpg';
+import Questions from './Questions.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="SHPE-logo" alt="logo" />
+        <h1> General Meeting Questions </h1>
+        <hr size="10px" width="85%" color="white"></hr>
+        <Questions />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>

37888a8be2d1468f4e4327bb327ab1c9c9bb0036
Martin Heberling <martin.heberling.dalies@gmail.com>
2021-10-22 00:13:00 -0400

    Added textbox and handlers.

--- src/App.js
+++ src/App.js
@@ -10,17 +10,6 @@ function App() {
         <h1> General Meeting Questions </h1>
         <hr size="10px" width="85%" color="white"></hr>
         <Questions />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
--- src/Questions.js
+++ src/Questions.js
@@ -5,7 +5,8 @@ class Questions extends React.Component {
     constructor(props) {
         super(props);
     this.state = {
-      value: ''
+      value: '',
+      questions: []
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -18,6 +19,8 @@ class Questions extends React.Component {
     
     handleSubmit(event) {
         alert('Your question was submitted: ' + this.state.value);
+        this.setState(prevState => ({questions: [...prevState.questions, [this.state.value]]}));
+        // console.log(this.state.questions);
         event.preventDefault();
     }
     
@@ -25,7 +28,7 @@ class Questions extends React.Component {
       return (
         <form onSubmit={this.handleSubmit}>
           <label>
-            Question:
+            Question: {console.log(this.state.questions)}
             <input type="text" value={this.state.value} onChange={this.handleChange} />
           </label>
           <input type="submit" value="Submit" />
